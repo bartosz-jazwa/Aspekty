cbf2b797422c847b057714f656523e1ce976c690
Bartosz Jazwa <bartosz.jazwa@gmail.com>
2019-04-17 18:21:59 +0200

    Pierszy push

--- main/java/com/jazwa/aspekty/model/CompanyAspect.java
+++ main/java/com/jazwa/aspekty/model/CompanyAspect.java
@@ -5,7 +5,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
